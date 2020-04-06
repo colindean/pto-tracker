e106a9922ed856b8930a99e46f834222a6b198ca
Colin Dean <colin.dean@target.com>
2020-04-06 14:45:25 -0400

    Adds a way to show the future projected PTO already recorded

--- pto.sh
+++ pto.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 BREW="/usr/local/bin/brew"
 LEDGER="ledger"
+
+
 if [[ -z "$(command -v $LEDGER)" ]]; then
   LEDGER="$($BREW --prefix $LEDGER)/bin/ledger"
   if [[ ! -e $LEDGER ]]; then
@@ -13,16 +15,29 @@ fi
 YEAR="$(date +%Y)"
 NOW="$(date +%Y/%m/%d)"
 
+END=""
+case "${1}" in
+  f|fut|future)
+    END=""
+    ;;
+  *)
+    END="--end ${NOW}"
+    ;;
+esac
+
 SCRIPT_DIR="$(cd "$(dirname "$(readlink "${BASH_SOURCE[0]}")")" && pwd)"
 
 LEDGER_FILE="$SCRIPT_DIR/$YEAR-pto.ledger"
 
+BEGIN="--begin '${YEAR}/01/01'"
+
 echo "Balance:"
 
-$LEDGER -f "$LEDGER_FILE" --begin "$YEAR/01/01" --end "$NOW" bal --effective --empty --force-color --color
+$LEDGER -f "$LEDGER_FILE" ${END} bal --effective --empty --force-color --color --budget
 
 echo "Usage:"
 
-$LEDGER -f "$LEDGER_FILE" --begin "$YEAR/01/01" --end "$NOW" reg --effective --empty --force-color --color --columns 72
+$LEDGER -f "$LEDGER_FILE" ${END} reg --effective --empty --force-color --color --columns 72 --budget not payee "Budget transaction"
 
+echo ""
 echo "Edit $LEDGER_FILE to change things."
